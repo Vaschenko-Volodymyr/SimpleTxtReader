7b31f8daa3c7a28245f9d686e8c54c547b1b9bb5
Vaschenko-Volodymyr <vaschenko.volodymyr@gmail.com>
2015-12-28 03:20:28 +0200

    Improvement of appearance

--- app/src/main/java/com/hardtechnologycreator/simplefilemanager/MainActivity.java
+++ app/src/main/java/com/hardtechnologycreator/simplefilemanager/MainActivity.java
@@ -1,8 +1,6 @@
 package com.hardtechnologycreator.simplefilemanager;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,9 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,21 +18,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_PATH = 1;
-    private final static int REQUEST_FILE_NAME = 2;
+    private final static int REQUEST_FILE_NAME = 100;
     String absoluteFilePath = "";
     String filesContent = "";
     private static String filename = "";
 
-    TextView tv;
-    Button btnClear, btnWrite;
+    TextView tvMain, tvHeader, tvFileLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        tv = (TextView) findViewById(R.id.tvMain);
-
-        Toast.makeText(this, String.valueOf(System.currentTimeMillis()).replaceAll("\\W", ""), Toast.LENGTH_LONG).show();
+        tvMain = (TextView) findViewById(R.id.tvMain);
+        tvHeader = (TextView) findViewById(R.id.tvHeader);
+        tvFileLocation = (TextView) findViewById(R.id.tvFileLocation);
 
         File fileName;
         String sdState = android.os.Environment.getExternalStorageState();
@@ -60,21 +52,20 @@ public class MainActivity extends AppCompatActivity {
         }
         if (!fileName.exists())
             fileName.mkdirs();
-        /*
-        String state = Environment.getExternalStorageState();
 
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            //SDcard is available
-            File f = new File("/sdcard/SimpleFileManager/test.txt");
-            if (!f.exists()) {
-                //File does not exists
-                try {
-                    f.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }*/
+        try {
+            File myFile = new File("/sdcard/SimpleFileManager/File.txt");
+            myFile.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(myFile);
+            OutputStreamWriter myOutWriter =
+                    new OutputStreamWriter(fOut);
+            myOutWriter.append(tvMain.getText());
+            myOutWriter.close();
+            fOut.close();
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -85,54 +76,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clearTextViewText();
-            }
-        });
-
-        btnWrite = (Button) findViewById(R.id.btnWriteToFile);
-        btnWrite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = tv.getText().toString();
-                saveFile(text);
-            }
-        });
     }
 
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        // See which child activity is calling us back.
+
         if (requestCode == REQUEST_PATH){
-            tv.setTextColor(getResources().getColor(R.color.black));
+            tvMain.setTextColor(getResources().getColor(R.color.black));
             if (resultCode == RESULT_OK) {
-                String fileContent = "Current file: SDCard";
+
                 absoluteFilePath = data.getStringExtra("AbsolutePath");
                 String path = data.getStringExtra("GetPath");
                 String name = data.getStringExtra("GetFileName");
-                fileContent += absoluteFilePath;
-                fileContent = fileContent  + "\nFile context: " + "\n";
+                String fileContent = "";
+
+                tvHeader.setText("File: " + name);
+                tvFileLocation.setText("Path: ../" + path);
+                tvMain.setText("");
                 fileContent += readFileSD(path, name);
-                fileContent = fileContent + "\nEnd of file." + "\n\n";
                 filesContent += fileContent;
-                tv.setText(filesContent);
+                tvMain.setText(filesContent);
             } else if (resultCode == RESULT_CANCELED) {
-                tv.clearComposingText();
-                tv.setTextColor(getResources().getColor(R.color.warning));
-                tv.setText(R.string.declaringOpeningNotTxtFiles);
+                tvMain.clearComposingText();
+                tvMain.setTextColor(getResources().getColor(R.color.warning));
+                tvMain.setText(R.string.declaringOpeningNotTxtFiles);
             }
         } else if (requestCode == REQUEST_FILE_NAME) {
             if (resultCode == RESULT_OK) {
-                filename = data.getStringExtra("FileName");
+                if (data != null) {
+                    filename = data.getStringExtra("FileName");
+                } else Toast.makeText(this, "data is null", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     private String readFileSD(String path, String name) {
-
         String result = "";
 
         if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
@@ -154,24 +132,16 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
-    private boolean saveFile() {
-        String fileName = String.valueOf(System.currentTimeMillis()).replaceAll("\\W", "");
-        return saveFile(fileName);
-    }
-
     private boolean saveFile(String fileName) {
         try {
-            File myFile = new File("/sdcard/SimpleFileManager/File.txt");
+            File myFile = new File("/sdcard/SimpleFileManager/" + fileName + ".txt");
             myFile.createNewFile();
             FileOutputStream fOut = new FileOutputStream(myFile);
             OutputStreamWriter myOutWriter =
                     new OutputStreamWriter(fOut);
-            myOutWriter.append(tv.getText());
+            myOutWriter.append(tvMain.getText());
             myOutWriter.close();
             fOut.close();
-            /*Toast.makeText(getBaseContext(),
-                    fileName,
-                    Toast.LENGTH_SHORT).show();*/
             return true;
         } catch (Exception e) {
             Toast.makeText(getBaseContext(), e.getMessage(),
@@ -182,35 +152,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     private void clearTextViewText () {
-        tv.setTextColor(getResources().getColor(R.color.tvMainColor));
-        tv.setText(R.string.tvMainText);
+        tvMain.setTextColor(getResources().getColor(R.color.tvMainColor));
+        tvMain.setText(R.string.tvMainText);
+        tvFileLocation.setText("");
+        tvHeader.setText(getResources().getString(R.string.tvHeaderText));
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             clearTextViewText();
             return true;
         }
-        if (id == R.id.action_save) {
-            Intent intent = new Intent(this, SaveFileActivity.class);
-            startActivityForResult(intent, REQUEST_FILE_NAME);
-
-            if (filename.equals("")) filename = String.valueOf(System.currentTimeMillis()).replaceAll("\\W", "");
-            saveFile(filename);
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
